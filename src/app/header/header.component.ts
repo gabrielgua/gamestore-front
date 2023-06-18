661984608b48214dcab402464dd9f8c7caec8b7d
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2023-06-18 04:03:34 -0300

    refazendo o header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
+import { FadeFromTop } from '../animations/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  animations: [FadeFromTop]
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
+
+  @ViewChild('header') header!: ElementRef;
+
+  constructor(private changeDetector: ChangeDetectorRef) {}
+  
+
+  breakToMobileWidth = 800;
+  mobile: boolean = false;
+  showMobileMenu: boolean = false;
+
+  ngAfterViewInit(): void {
+    this.manageScreenSize(this.header.nativeElement.offsetWidth);
+    this.changeDetector.detectChanges();
+  }
+
+  manageScreenSize(width: number): void {
+    const isMobile = width <= this.breakToMobileWidth;
+    this.mobile = isMobile ? true : false;   
+
+    this.showMobileMenu = this.showMobileMenu && !isMobile ? false : this.showMobileMenu;
+  }
+
 
 }
