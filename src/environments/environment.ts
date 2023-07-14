cefacf19ba3c44d0fd0d6daad121c1e746068cc7
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2023-07-14 15:55:54 -0300

    adding .env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production: false,
-    API_URL: 'https://0a8a-201-89-126-83.ngrok-free.app',
-    TOKEN_ALLOWED_DOMAINS: [ /localhost:8080/ ],
-
+    API_URL: process.env['API_URL'],
 }
