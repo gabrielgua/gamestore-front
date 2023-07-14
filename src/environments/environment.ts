f36efef97e6c2f123563d9ab7864d34c7438fb96
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2023-07-14 15:15:02 -0300

    ngrok testing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    API_URL: 'http://localhost:8080',
+    API_URL: 'https://e3ac-201-89-126-83.ngrok-free.app',
     TOKEN_ALLOWED_DOMAINS: [ /localhost:8080/ ],
 
 }
