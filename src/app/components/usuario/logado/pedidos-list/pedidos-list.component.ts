bdc79c045f2f6f70839415649892b04ff574c3fd
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2023-07-18 17:17:30 -0300

    refatorando métodos de fetch no componente de listagem de pedidos

--- src/app/components/usuario/logado/pedidos-list/pedidos-list.component.ts
+++ src/app/components/usuario/logado/pedidos-list/pedidos-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
-import { Observable, Subscription, catchError, map, switchMap, tap } from 'rxjs';
+import { Observable, Subscriber, Subscription, catchError, ignoreElements, of, tap } from 'rxjs';
 import { Animations } from 'src/app/animations/animations';
 import { Pedido } from 'src/app/models/pedidos/pedido';
 import { StatusPedido } from 'src/app/models/pedidos/status.pedido';
@@ -13,38 +13,88 @@ import { PedidoService } from 'src/app/service/pedidos/pedido.service';
   animations: [Animations]
 })
 export class PedidosListComponent implements OnInit, OnDestroy {
+  
+  pedidos$ = new Observable<Pedido[]>;
+  pedidosError$ = new Observable<any>;
 
+  actionSub = new Subscription();
 
   constructor(private pedidoService: PedidoService, private toastr: ToastrService) {}
   
-  pedidos: Pedido[] = [];
-  isLoading$ = new Observable<boolean>;
-  pedidosSub = new Subscription();
-  error: boolean = false;
- 
+
   ngOnInit(): void {
-    this.pedidoService.resetPedidos();
-    this.pedidoService.fetchUsuarioLogadoPedidos();
-    this.getPedidos();
+    this.getPedidos();    
   }
 
   ngOnDestroy(): void {
-    this.pedidosSub.unsubscribe();    
+    this.actionSub.unsubscribe();
+  }
+  
+  private getPedidos(): void {
+    this.pedidos$ = this.pedidoService.fetchUsuarioLogadoPedidos();
+    this.pedidosError$ = this.pedidos$.pipe(
+      ignoreElements(),
+      catchError((err) => of(err))
+    );
   }
 
-  private getPedidos() {
-    this.pedidosSub = this.pedidoService.getPedidos()
-      .subscribe(pedidos => this.pedidos = pedidos);
+  public confirmarPedido(pedido: Pedido): void {
+    this.startPedidoLoading(pedido);
+    this.actionSub = this.pedidoService.confirmarPedido(pedido)
+      .subscribe({
+        next: () => this.handleActionSuccess(pedido, 'confirmado'),
+        error: (err) => this.handleActionError(pedido, 'confirmado', err.error.title),
+      })
   }
 
-  public getIsLoading(): Observable<boolean> {
-    return this.pedidoService.isLoadingPedidos;
+  public cancelarPedido(pedido: Pedido): void {
+    this.startPedidoLoading(pedido);
+    this.actionSub = this.pedidoService.cancelarPedido(pedido)
+      .subscribe({
+        next: () => this.handleActionSuccess(pedido, 'cancelado'),
+        error: (err) => this.handleActionError(pedido, 'cancelado', err.error.title),
+      })
   }
 
-  public getIsError(): Observable<boolean> {
-    return this.pedidoService.error;
+  public reembolsarPedido(pedido: Pedido): void {
+    this.startPedidoLoading(pedido);
+    this.actionSub = this.pedidoService.reembolsarPedido(pedido)
+      .subscribe({
+        next: () => this.handleActionSuccess(pedido, 'reembolsado'),
+        error: (err) => this.handleActionError(pedido, 'reembolsado', err.error.title),
+      })
   }
  
+
+  private handleActionSuccess(pedido: Pedido, action: string) {
+    this.getPedidos();
+    this.stopPedidoLoading(pedido);
+    this.toastrSuccess(`Pedido ${action} com sucesso.`);
+  }
+
+  private handleActionError(pedido: Pedido, action: string, title: string) {
+    this.getPedidos();
+    this.stopPedidoLoading(pedido);
+    this.toastrError(`Pedido não pode ser ${action}.`, title);
+  }
+
+
+  private startPedidoLoading(pedido: Pedido) {
+    pedido.isLoadingActions = true;
+  }
+
+  private stopPedidoLoading(pedido: Pedido) {
+    pedido.isLoadingActions = false;
+  }
+
+  private toastrError(message: string, title: string): void {
+    this.toastr.error(message, title);
+  }
+
+  private toastrSuccess(message: string, title?: string): void {
+    this.toastr.success(message, title);
+  }
+
   public isPositive(status: StatusPedido): boolean {
     return status === StatusPedido.CONFIRMADO || status === StatusPedido.CRIADO;
   }
@@ -64,18 +114,5 @@ export class PedidosListComponent implements OnInit, OnDestroy {
   public isCanceladoOuReembolsado(status: StatusPedido): boolean {
     return status === StatusPedido.CANCELADO || status === StatusPedido.REEMBOLSADO;
   }
-
-  public confirmarPedido(pedido: Pedido): void {
-    this.pedidoService.confirmarPedido(pedido);
-  }
-
-  public cancelarPedido(pedido: Pedido): void {
-    this.pedidoService.cancelarPedido(pedido);
-  }
-
-  public reembolsarPedido(pedido: Pedido): void {
-    this.pedidoService.reembolsarPedido(pedido);
-  }
- 
 
 }
--- src/app/service/pedidos/pedido.service.ts
+++ src/app/service/pedidos/pedido.service.ts
@@ -1,144 +1,37 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, OnDestroy, OnInit } from '@angular/core';
-import { subscribe } from 'diagnostics_channel';
-import { ToastrService } from 'ngx-toastr';
-import { BehaviorSubject, Observable, Subscription, catchError, firstValueFrom, tap } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Pedido } from 'src/app/models/pedidos/pedido';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PedidoService implements OnDestroy {
-
-  private pedidos$ = new BehaviorSubject<Pedido[]>({} as Pedido[]);
-  private isLoadingPedidos$ = new BehaviorSubject<boolean>(false);
-  private error$ = new BehaviorSubject<boolean>(false);
-
-  isLoadingPedidos = this.isLoadingPedidos$.asObservable();
-  error = this.error$.asObservable();
-  pedidosSub = new Subscription();
+export class PedidoService  {
 
 
-  constructor(private http: HttpClient, private toastr: ToastrService) { }
+  constructor(private http: HttpClient) { }
   
-  ngOnDestroy(): void {
-    this.pedidosSub.unsubscribe();
-  }
-
-  public getPedidos() {
-    return this.pedidos$.asObservable();
-  }
-
-  public resetPedidos() {
-    this.pedidos$.next([]);
-  }
-
-  private loadingNoError() {
-    this.isLoadingPedidos$.next(true);
-    this.error$.next(false);
-  }
-
-  public setPedidos(pedidos: Pedido[]) {
-    this.pedidos$.next(pedidos);
-  }
   
-  fetchUsuarioLogadoPedidos() {
-    this.loadingNoError();
-    this.resetPedidos();
-
-    this.http.get<Pedido[]>(`${environment.API_URL}/usuarios/meus-pedidos`)
-      .subscribe({
-        next: pedidos => {
-          this.pedidos$.next(pedidos);
-          this.isLoadingPedidos$.next(false); 
-        },
-        error: error => {
-          console.log(error);
-          this.isLoadingPedidos$.next(false); 
-          this.error$.next(true);
-        }
-      });
+  fetchUsuarioLogadoPedidos(): Observable<Pedido[]> {
+    return this.http.get<Pedido[]>(`${environment.API_URL}/usuarios/meus-pedidos`);
   }
 
   fetchAllPedidos() {
-    this.loadingNoError();
-    this.resetPedidos();
+    return this.http.get<Pedido[]>(`${environment.API_URL}/pedidos`);
+  }
 
-    this.http.get<Pedido[]>(`${environment.API_URL}/pedidos`)
-      .subscribe({
-        next: pedidos => {
-          this.pedidos$.next(pedidos);
-          this.isLoadingPedidos$.next(false); 
-        },
-        error: error => {
-          console.log(error);
-          this.isLoadingPedidos$.next(false); 
-          this.error$.next(true);
-        }
-      });
+  cancelarPedido(pedido: Pedido) {
+    return this.http.put<void>(`${environment.API_URL}/pedidos/${pedido.codigo}/cancelar`, {});
+  }
+
+  confirmarPedido(pedido: Pedido) {
+    return this.http.put<void>(`${environment.API_URL}/pedidos/${pedido.codigo}/confirmar`, {})
+  }
+
+  reembolsarPedido(pedido: Pedido) {
+    return this.http.put<void>(`${environment.API_URL}/pedidos/${pedido.codigo}/reembolsar`, {})
   }
 
   
-
-  cancelarPedido(pedido: Pedido): void {
-    pedido.isLoadingActions = true;
-    this.http.put<void>(`${environment.API_URL}/pedidos/${pedido.codigo}/cancelar`, {})
-      .subscribe({
-        next: () => {
-          this.fetchUsuarioLogadoPedidos();
-          pedido.isLoadingActions = false;
-        },
-        error: (error) => {
-          pedido.isLoadingActions = false;
-          if (error.error.status === 400) {
-            this.handleActionError('Pedido não pode ser cancelado.', error.error.title);
-          } else {
-            this.handleActionError('Erro ao cancelar pedido', error.error.title);
-          }
-        }
-      });
-  }
-
-  confirmarPedido(pedido: Pedido): void {
-    pedido.isLoadingActions = true;
-    this.http.put<void>(`${environment.API_URL}/pedidos/${pedido.codigo}/confirmar`, {})
-    .subscribe({
-      next: () => {
-        this.fetchUsuarioLogadoPedidos();
-        pedido.isLoadingActions = false;
-      },
-      error: (error) => {
-        pedido.isLoadingActions = false;
-        if (error.error.status === 400) {
-          this.handleActionError('Pedido não pode ser confirmado.', error.error.title);
-        } else {
-          this.handleActionError('Erro ao confirmar pedido', error.error.title);
-        }
-      }
-    });
-  }
-
-  reembolsarPedido(pedido: Pedido): void {
-    pedido.isLoadingActions = true;
-    this.http.put<void>(`${environment.API_URL}/pedidos/${pedido.codigo}/reembolsar`, {})
-    .subscribe({
-      next: () => {
-        this.fetchUsuarioLogadoPedidos();
-        pedido.isLoadingActions = false;
-      },
-      error: (error) => {
-        pedido.isLoadingActions = false;
-        if (error.error.status === 400) {
-          this.handleActionError('Pedido não pode ser reembolsado.', error.error.title);
-        } else {
-          this.handleActionError('Erro ao reembolsar pedido', error.error.title);
-        }
-      }
-    });
-  }
-
-  private handleActionError(message: string, title: string): void {
-    this.toastr.error(message, title);
-  }
 }
