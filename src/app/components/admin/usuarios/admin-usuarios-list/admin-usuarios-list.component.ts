03c9ec0f3d94a3ed151d60c57195554ff33a2194
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2023-09-11 16:57:42 -0300

    implementando tabela de listagem de usuários

--- src/app/components/admin/usuarios/admin-usuarios-list/admin-usuarios-list.component.ts
+++ src/app/components/admin/usuarios/admin-usuarios-list/admin-usuarios-list.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Usuario } from 'src/app/models/usuarios/usuario';
+import { UsuarioListService } from 'src/app/service/usuarios/usuario-list.service';
 
 @Component({
   selector: 'app-admin-usuarios-list',
   templateUrl: './admin-usuarios-list.component.html',
   styleUrls: ['./admin-usuarios-list.component.css']
 })
-export class AdminUsuariosListComponent {
+export class AdminUsuariosListComponent implements OnInit {
 
   usuarios$ = new Observable<Usuario[]>();
 
-  constructor() {}
+  constructor(private service: UsuarioListService) {}
 
-  
+
+  ngOnInit(): void {
+    this.service.fetchUsuarios();
+    this.usuarios$ = this.service.getUsuarios();
+  }
 }
--- src/app/service/usuarios/usuario-list.service.ts
+++ src/app/service/usuarios/usuario-list.service.ts
@@ -1,14 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Usuario } from 'src/app/models/usuarios/usuario';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioListService {
 
-  usuarios$ = new BehaviorSubject<Usuario[]>([]);
+  private usuarios$ = new BehaviorSubject<Usuario[]>([]);
 
   constructor(private http: HttpClient) { }
-}
+
+
+  public getUsuarios(): Observable<Usuario[]> {
+    return this.usuarios$.asObservable();
+  }
+
+  fetchUsuarios(): void {
+    this.http.get<Usuario[]>(`${environment.API_URL}/usuarios`)
+      .subscribe(usuarios => this.usuarios$.next(usuarios));
+  }
+} 
